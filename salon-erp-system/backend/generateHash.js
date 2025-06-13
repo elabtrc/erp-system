04c15da4af5c9bfc78ecd46189797483e419afc0
elabtrc <riegobeatrice@gmail.com>
2025-06-13 19:34:59 +0800

    Switched from bcrypt to bcryptjs

--- salon-erp-system/backend/generateHash.js
+++ salon-erp-system/backend/generateHash.js
@@ -1,11 +1,11 @@
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
 // Generate new hashes
 async function generateHashes() {
-  console.log('Branch Manager:', await bcrypt.hash('branch123', saltRounds));
-  console.log('Accountant:', await bcrypt.hash('account456', saltRounds));
-  console.log('Receptionist:', await bcrypt.hash('recep789', saltRounds));
-  console.log('Super Admin:', await bcrypt.hash('superadmin', saltRounds));
+  console.log('Branch Manager:', await bcryptjs.hash('branch123', saltRounds));
+  console.log('Accountant:', await bcryptjs.hash('account456', saltRounds));
+  console.log('Receptionist:', await bcryptjs.hash('recep789', saltRounds));
+  console.log('Super Admin:', await bcryptjs.hash('superadmin', saltRounds));
 }
 generateHashes();
